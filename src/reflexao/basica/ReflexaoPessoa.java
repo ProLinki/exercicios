3d43700f1df4ae77c571d5896fbf0a2ce5a08959
ProLinki <llas140594@gmail.com>
2016-03-08 04:41:41 -0300

    Update ReflexaoPessoa.java

--- src/reflexao/basica/ReflexaoPessoa.java
+++ src/reflexao/basica/ReflexaoPessoa.java
@@ -11,13 +11,13 @@ public class ReflexaoPessoa {
     public static void main(String[] args) throws IllegalArgumentException, IllegalAccessException {
         /*Criar um objeto do tipo Pessoa.*/
         Pessoa pessoa = new Pessoa();
-        /*Pegando a classe, para observar utlizar e/ou utilizar seu atributos e métodos.*/
+        /*Pegando a classe, para observar e/ou utilizar seu atributos e métodos.*/
         Class<?> classe = pessoa.getClass();
-        /*Pessoa todos campos declarados.*/
+        /*Todos os campos declarados.*/
         Field[] fieldName = classe.getDeclaredFields();
         /*Acessando cada índice e verificando se corresponde ao campo nome.*/
         for (Field f : fieldName) {
-            /*Caso verdadeiro, atribui acesso ao campo e seta um valor passando como parametro 
+            /*Caso verdadeiro, atribui acesso ao campo e seta um valor passado como parametro 
             o objeto e o valor a ser inserido.*/
             if (f.getName().equals("nome")) {
                 f.setAccessible(true);
